7f08843a3bdd797091a03202634e0d2e02806dd9
vbtech959 <vbtech959@gmail.com>
2024-12-06 21:15:06 -0300

    Update exemplo.js

---
+++ exemplo.js
@@ -0,0 +1,5 @@
+let filmes= [
+    {id: 1, titulo: "Por lugares incríveis", preco: 35.00, estoque: 10} ,
+    {id: 2, titulo: "Pânico IV", preco: 40.00, estoque: 5},
+    {id: 3, titulo: "Corra!", preco: 39.43, estoque: 12}
+]
